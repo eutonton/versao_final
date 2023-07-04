33610ae87b98361d6304715dbfec978161fbd02e
Wellington Araújo <jwellington427@gmail.com>
2023-07-04 18:07:14 -0300

    Add files via upload

---
+++ back-end-interclass-main-main/Back_and_trabalho_pdo-main/desgin/home.php
@@ -0,0 +1,139 @@
+<?php 
+
+include_once "../conexao_pdo.php";
+
+$sql = "SELECT * FROM empresas";
+
+try {
+    $stmt = $pdo->query($sql);
+    $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    echo 'Erro ao ler registros: ' . $e->getMessage();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <title>Home</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<header class="header">
+
+   <section class="flex">
+
+      <div id="menu-btn" class="fas fa-bars-staggered"></div>
+
+      <a href="home.php" class="logo"><i class="fas fa-briefcase"></i> Emprego sem fronteiras</a>
+
+      <nav class="navbar">
+         <a href="home.php">home</a>
+         <a href="jobs.php">Vagas</a>
+         <a href="nossaequipe.html">Nossa equipe</a>
+      </nav>
+
+      <a  class="btn" style="margin-top: 0; background-color: transparent">ㅤㅤㅤㅤㅤㅤㅤㅤㅤ</a>
+
+   </section>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<div class="home-container">
+
+   <section class="home">
+
+      :
+
+   </section>
+
+</div>
+
+<!-- home section ends -->
+
+<!-- category section starts  -->
+
+
+
+<!-- category section ends -->
+
+<!-- jobs section starts  -->
+
+<section class="jobs-container">
+    <h1 class="heading">Vagas</h1>
+
+    <div class="box-container">
+
+        <?php
+        foreach ($resultados as $row) {
+            echo '<div class="box">';
+            echo '<div class="company">';
+            echo '<img src="./images/maleta.png.png" alt="">';
+            echo '<div>';
+            echo '<h3>' . $row['nome'] . '</h3>';
+            echo '<p>2 days ago</p>';
+            echo '</div>';
+            echo '</div>';
+            echo '<h3 class="job-title">' . $row['funcao'] . '</h3>';
+            echo '<p class="location"><i class="fas fa-map-marker-alt"></i> <span>' . $row['endereco'] . '</span></p>';
+            echo '<div class="tags">';
+            echo '<p><i class="fa-solid fa-brazilian-real-sign"></i> <span>' . $row['faixa_salario'] . '</span></p>';
+            echo '<p><i class="fas fa-briefcase"></i> <span>' . $row['carga'] . '</span></p>';
+            // echo '<p><i class="fas fa-clock"></i> <span>day shift</span></p>';
+            echo '</div>';
+            echo '<div class="flex-btn">';
+            echo '<a href="../front_cadidatura copy.php" class="btn">Canditar-se</a>';
+            echo '<button type="submit" class="far fa-heart" name="save"></button>';
+            echo '</div>';
+            echo '</div>';
+        }
+        ?>
+
+    </div>
+</section>
+
+<!-- jobs section ends -->
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <div class="credit">&copy; copyright @ 2023 by <span>Team Emprego sem Fronteiras</span> | Todos os direitos reservados!</div>
+
+</footer>
+
+<!-- footer section ends -->
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+   
+</body>
+</html>
